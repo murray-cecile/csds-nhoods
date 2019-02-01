23b4859fd190c8bf697a4b5792e61574183ca32b
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-02-01 10:37:03 -0600

    starting work on osg stuff

--- practice_maps.py
+++ practice_maps.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib
 import sys
 import pylab
+import pytz
 
 # setup
 datadir = "processed/"
@@ -16,13 +17,19 @@ shpdir = "tracts/"
 city = sys.argv[1]
 pylab.ion()
 
+timezone = {"new_york" : "Eastern", "los_angeles" : "Pacific", "chicago" : "Central", "houston" : "Central", "phoenix" : "Mountain",
+            "philadelphia" : "Eastern", "san_antonio" : "Central", "san_diego" : "Pacific", "dallas" : "Central", "san_jose" : "Pacific",
+            "austin" : "Central", "jacksonville" : "Eastern", "san_francisco" : "Pacific", "columbus" : "Eastern", "fort_worth" : "Central",
+            "indianapolis" : "Eastern", "charlotte" : "Eastern", "seattle" : "Pacific", "denver" : "Mountain", "washington" : "Eastern"}
+
+
 # read in data
 df = pd.read_csv(datadir + city + "_users.csv")
-fulldf = pd.read_csv(datadir + city + "_000.csv", columns = ["uid", "timestamp", "tract", "lat", "lon", "acc", "hwy"])
+fulldf = pd.read_csv(datadir + city + "_000.csv", names = ["uid", "timestamp", "tract", "lat", "lon", "acc", "hwy"])
 map_df = gpd.read_file(shpdir + city + ".geojson")
 
 # hey look, it's a city
-map_df.plot()
+# map_df.plot()
 
 # collapse this weird 11-tract data frame
 sumdf = df.groupby('home', as_index=False).agg('count')
@@ -30,8 +37,14 @@ sumdf.home = sumdf['home'].astype(str)
 
 # now merge in data
 merged = map_df.merge(sumdf, left_on = "GEOID", right_on = "home")
-merged.plot(column = "uid")
+# merged.plot(column = "uid")
 
 # okay now let's do it with the bigger one
-aggdf = fulldf.groupby('home', as_index=False).agg('count')
-aggdf
+aggdf = fulldf.groupby('tract', as_index=False).agg('count')
+aggdf.tract = aggdf.tract.astype(str)
+merged2 = map_df.merge(aggdf, left_on='GEOID', right_on = 'tract')
+# merged2.plot(column = "uid")
+
+fulldf['ts'] = pd.to_datetime(fulldf.timestamp, unit = "s").dt.tz_localize('utc').dt.tz_convert(pytz.timezone('US/' + timezone[city]))
+
+fulldf.uid.unique()
--- processing_natl.py
+++ processing_natl.py
@@ -1,5 +1,6 @@
 #==================================================================#
-# PROCESSING - copy from jsaxon/parks
+# PROCESSING - version for whole country
+# this was a terrible idea
 # Cecile Murray
 #==================================================================#
 
@@ -14,29 +15,6 @@ import os
 import csv
 import sys
 
-
-cities = ["new_york", "los_angeles", "chicago", "houston", "phoenix", 
-          "philadelphia", "san_antonio", "san_diego", "dallas", "san_jose", 
-          "austin", "jacksonville", "san_francisco", "columbus", "fort_worth", 
-          "indianapolis", "charlotte", "seattle", "denver", "washington"]
-
-# cities = ["philadelphia"]
-
-epsg   = {"new_york" : 3623, "los_angeles" : 3488, "chicago" : 3528, "houston" : 3665, "phoenix" : 3478, 
-          "philadelphia" : 3364, "san_antonio" : 3665, "san_diego" : 3488, "dallas" : 3665, "san_jose" : 3488, 
-          "austin" : 3665, "jacksonville" : 3513, "san_francisco" : 3488, "columbus" : 3637, "fort_worth" : 3665, 
-          "indianapolis" : 3532, "charlotte" : 3631, "seattle" : 3689, "denver" : 3501, "washington" : 3689}
-
-
-def ens_dir(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        print(file_path)
-        
-for c in cities:
-    ens_dir("processed/" + c + "/")
-
 def cut_box(row, x1, y1, x2, y2):
 
   try:
@@ -46,31 +24,20 @@ def cut_box(row, x1, y1, x2, y2):
     print(row)
     sys.exit()
 
-def main(n = 0, c = "philadelphia"):
-
-    if n in [58, 403]: return
-
-    print("Now processing {}, job {}.".format(c, n))
-
-    # read in city-level geojson file
-    pl = gpd.read_file("places/{}_10km.geojson".format(c)).to_crs(epsg = 4326)
-    places = pl.ix[0].geometry
-    bounds = places.bounds
+def main(n = 0):
 
     # read in tract file
-    tracts = gpd.read_file("tracts/{}.geojson".format(c)).to_crs(epsg = 4326)
+    tracts = gpd.read_file("tracts/us_tracts.shp")
     tracts = tracts[["GEOID", "geometry"]].rename(columns = {"GEOID" : "geoid"})
 
     # read in OSM ways file
-    roads = gpd.read_file("ways/{}_way.geojson".format(c))
+    roads = gpd.read_file("ways/lines.shp")
     roads = gpd.GeoDataFrame(geometry = roads.buffer(10).to_crs(epsg = 4326))
     roads.index.name = "hway"
 
 
-    with open("processed/{}_{:03d}.csv".format(c, n), 'w') as f: pass
-
     # read through data in chunks of 1M
-    iter_csv = pd.read_csv("data/u000-2.csv", chunksize = 1e6,
+    iter_csv = pd.read_csv("data/u000.csv", chunksize = 1e3,
                           names = ["advertising_id", "timestamp", "latitude", "longitude", "accuracy"],
                            dtype = {"advertising_id" : str, "timestamp" : int, "latitude" : float,
                             "longitude" : float, "accuracy" : int})
@@ -82,16 +49,10 @@ def main(n = 0, c = "philadelphia"):
 
         # drop if the accuracy is too low or if the point is not inside city bounding lat/lon
         df.drop(df[df.accuracy == 0].index, inplace = True)
-        df.drop(df[~df.apply(cut_box, args = bounds, axis = 1)].index, inplace = True)
 
         # convert lat/lons to Point projected in local plane
         gs = gpd.GeoSeries(index = df.index, crs = fiona.crs.from_epsg(4326), 
                            data = [Point(xy) for xy in zip(df.longitude, df.latitude)])
-
-        # perform spatial join: point in place polygon
-        gdf = gpd.GeoDataFrame(data = df, geometry = gs)
-        gdf.drop(gdf[~gdf.intersects(places)].index, inplace = True)
-        gdf.reset_index(inplace = True, drop = True)
 
         # perform join on major roadways, so we can drop them later.
         gdf["hway"] = 0
@@ -103,35 +64,13 @@ def main(n = 0, c = "philadelphia"):
         gdf.advertising_id = gdf.advertising_id.str.lower()
 
         # write result to file
-        with open("processed/{}_{:03d}.csv".format(c, n), 'a') as f: 
+        with open("processed/natl_{:03d}.csv".format(n), 'a') as f: 
 
           gdf[["advertising_id", "timestamp", "geoid",
                "latitude", "longitude", "accuracy", "hway"]]\
              .to_csv(f, index = False, float_format='%.5f', header = False)
     
     # return gdf[["advertising_id", "timestamp", "geoid", "latitude", "longitude", "accuracy", "hway", "geometry"]]
-
-
-# import multiprocessing
-
-
-# def run_city_dict(d): main(**d)
-
-# def queue_cities():
-
-#   arg_list = []
-#   for c in cities:
-#     for n in range(500):
-#       arg_list.append({"n" : n, "c" : c})
-
-#   p = multiprocessing.Pool(12)
-#   p.map(run_city_dict, arg_list)
-
-
-
-# with open("processing.out", "w") as out: pass
-
-# main(n = 0, city = "philadelphia")
 
 import argparse
 
@@ -142,4 +81,4 @@ if __name__ == '__main__':
   parser.add_argument("-c", "--city", type = str, default = "philadelphia", help="City name")
   args = parser.parse_args()
 
-  main(n = args.num, c = args.city)
+  main(n = args.num)
