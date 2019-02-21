4fddfb01f2793321beca28e878a51b04f164fe5d
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-02-21 17:45:46 -0600

    script to merge states for one user id

--- concatenate_states.py
+++ concatenate_states.py
@@ -6,21 +6,29 @@
 #==================================================================#
 
 import pandas as pd 
+import argparse
 
 PROCESSED = "processed/"
-MERGED = "uid/"
-BY_STATE = "by-st/"
 
-STATES = ["01", "04", "05", "06", "08"] #, "02", "09", "10", "11", "12", "13",
-#  "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
-#   "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
-#   "41", "42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56"]
 
-def main():
+STATES = ["01", "04", "05", "06", "08", "02", "09", "10", "11", "12", "13",
+ "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
+  "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+  "41", "42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56"]
 
-    with open(PROCESSED + MERGED + 'u_{:02d}.csv'.format(j), "a") as f:
+def main(j):
+
+    with open(PROCESSED + 'u_{:02d}.csv'.format(j), "a") as f:
 
         for st in STATES:
 
-            df = pd.read_csv(PROCESSED + BY_STATE + 'u_{}_' + st + '.csv')
-            df.to_csv(f, index = False, float_format='%.5f', header = False)
+            df = pd.read_csv(PROCESSED + 'u_{:02d}/' + st + '.csv')
+            df.to_csv(f, index = False, float_format='%.5f', header = False, compression = 'bz2')
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j", "--num",  type = int, default = 0, help="A number!")
+    args = parser.parse_args()
+
+    main(j = args.num)
